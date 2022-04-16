e49a30ce0d2e57b7692dcd035165e9a508fc492c
vinicius-je <vinicius.je99@gmail.com>
2022-04-15 23:31:20 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Carousel>
+        <img src="https://images.unsplash.com/photo-1602033750341-71d8db6008a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="placeholder" />
+        <img src="https://via.placeholder.com/1200x500" alt="placeholder" />
+        <img src="https://via.placeholder.com/1200x500" alt="placeholder" />
+      </Carousel>
     </div>
   );
 }
---
+++ src/components/Carousel/index.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import './style.css'
+
+const Carousel = ({children}) => {
+    const [index, setIndex] = useState(0);
+    // amount of carousel items
+    const [items, setItems] = useState(children.length);
+
+    useEffect(() => {
+        setItems(children.length);
+    }, [children])
+
+    const nextSlide = () => {
+        if(index < (items - 1)){
+            setIndex(state => state + 1);
+        }
+    }
+
+    const prevSlide = () => {
+        if(index > 0){
+            setIndex(state => state - 1);
+        }
+    }
+
+    return(
+        <div className="carousel-container">
+            <button className="left-btn" onClick={prevSlide}>&lt;</button>
+            <div className="carousel-content-wrapper">
+                <div className="carousel-content"
+                    style={{transform: `translateX(-${index * 100}%)`}}>
+                    {children}
+                </div>
+            </div>
+            <button className="right-btn" onClick={nextSlide}>&gt;</button>
+        </div>
+    )
+}
+
+export default Carousel;
